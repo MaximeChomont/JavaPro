4dec86e824e73ae77c214982fdf042e8ced2dcb2
FabienCampredon <campredon.fab@hotmail.fr>
2018-02-13 18:31:17 +0100

    o

---
+++ apiIMPLEMENTATION/src/test/java/modele/Cascade/inter/ICascade.java
@@ -0,0 +1,10 @@
+package modele.Cascade.inter;
+
+import modele.Personne.inter.IPersonne;
+
+public interface ICascade {
+
+    public IPersonne getPersonne();
+
+    public void setPersonne(IPersonne personne);
+}
